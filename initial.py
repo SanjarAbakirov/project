5f4efb452daa1b4dbc452de45fa9140ca941fb29
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2025-10-15 16:27:28 +0600

    return a value

--- initial.py
+++ initial.py
@@ -35,3 +35,14 @@ def some_fn_from_me():
     # print("Hello, %s , from my Fn! with you %s"%(username, greetings))
 # my_fn_args("Sam", "we do many things and projects")
 
+# return values
+
+def sum_num(a, b):
+   return a + b
+
+def multy_num(a, b):
+   return a*b
+
+# 21*21 + 46*46
+
+print(sum_num(multy_num(21, 21), multy_num(46, 46)))
