8e7272586fc3229c9369493c88e480531902c21a
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2025-10-15 16:12:55 +0600

    HW_1 with Alice is done

--- hello.py
+++ hello.py
@@ -1,4 +1,6 @@
 # HW_W_1
+
+# Homework W1
 # Task_1
 
 # print("Hellow world")
@@ -17,3 +19,29 @@ def greetings_Alice():
     print("Somebody is calling Alice")
 greetings_Alice()
 
+# Task 3 change "Hello" for "Howdy"
+
+"""A tiny Python program to check that Python is working. Try running this program from the command line like this:
+  python hello.py
+  python hello.py Alice
+That should print:
+  Hello World -or- Hello Alice
+Try changing the 'Hello' to 'Howdy' and run again.
+Once you have that working, you're ready for class -- you can edit
+and run Python code; now you just need to learn Python!
+"""
+
+def greet_fn(name, extra_word):
+    print("Hello, " + name + ". Welcome and " + extra_word)
+
+# greet_fn("Sam", "Have a good day")
+
+# first way
+def function_alice(greeting, name):
+    print("%s, %s"%(greeting, name))
+function_alice("Hello", "Alice")
+
+# second way
+def fn_greet_alice(greeting, name):
+    print(greeting + ". " + name)
+fn_greet_alice("Howdy", "Alice")
--- initial.py
+++ initial.py
@@ -3,31 +3,21 @@
 #     print(f"Hello, {name}")
 # new_fn("Sam")
 
-# Homework W1
 
-"""A tiny Python program to check that Python is working. Try running this program from the command line like this:
-  python hello.py
-  python hello.py Alice
-That should print:
-  Hello World -or- Hello Alice
-Try changing the 'Hello' to 'Howdy' and run again.
-Once you have that working, you're ready for class -- you can edit
-and run Python code; now you just need to learn Python!
-"""
 
-# import sys
-# # Define a main() - the function that prints a little greeting.
-# def main():
-#   # Get the name from the command line, using 'World' as a fallback.
-#   if len(sys.argv) >= 2:
-#     name = sys.argv[1]
-#   else:
-#     name = 'Alice'
-#   print('Hello', name)
+import sys
+# Define a main() - the function that prints a little greeting.
+def main():
+  # Get the name from the command line, using 'World' as a fallback.
+  if len(sys.argv) >= 2:
+    name = sys.argv[1]
+  else:
+    name = 'Alice'
+  print('Hello', name)
 
-# # This is the standard boilerplate that calls the main() function.
-# if __name__ == '__main__':
-#   main()
+# This is the standard boilerplate that calls the main() function.
+if __name__ == '__main__':
+  main()
 
 
 def my_function():
@@ -43,20 +33,5 @@ def some_fn_from_me():
 # pattern
 # def my_fn_args(username, greetings):
     # print("Hello, %s , from my Fn! with you %s"%(username, greetings))
-
 # my_fn_args("Sam", "we do many things and projects")
 
-def greet_fn(name, extra_word):
-    print("Hello, " + name + ". Welcome and " + extra_word)
-
-# greet_fn("Sam", "Have a good day")
-
-# first way
-def function_alice(greeting, name):
-    print("%s, %s"%(greeting, name))
-function_alice("Hello", "Alice")
-
-# second way
-def fn_greet_alice(greeting, name):
-    print(greeting + ". " + name)
-fn_greet_alice("Howdy", "Alice")
