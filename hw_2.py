aa1dbc596515efa56b9b433b04d9ebe2715b58a5
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2025-10-17 12:35:55 +0600

    the new iterration of main()

--- hw_2.py
+++ hw_2.py
@@ -1,18 +1,21 @@
 import sys
 
 def main():
-  
-  if len(sys.argv) >= 2:
-    name = sys.argv[1]
+  question = input('How many donuts?')
+  num_don = 0
+  if num_don(sys.argv[1]) >= 9:
+    # num_don = sys.argv[1]
+    print("Number of donuts: " + num_don)
   else:
-    name = 'World'
-  print('Greetings', name)
+    print("many")
 
 if __name__ == '__main__':
-  main()
 # Код, который выполняется только при прямом запуске (например, main()),
 # и код (функции, классы), который можно использовать при импорте, не вызывая побочных эффектов.
 # «Если этот файл запущен напрямую (а не импортирован), то вызови функцию main()».
+  main()
+
+
 
 # def sum_num(a, b):
 #    return a + b
