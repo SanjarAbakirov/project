c34789d27bfff6c4509cb0360f559b95a6c638bd
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2025-10-17 12:28:20 +0600

    main() it works

--- hw_2.py
+++ hw_2.py
@@ -1,20 +1,18 @@
-# import sys
+import sys
 
-# def main():
+def main():
   
+  if len(sys.argv) >= 2:
+    name = sys.argv[1]
+  else:
+    name = 'World'
+  print('Greetings', name)
 
-
-
-#   if len(sys.argv) >= 2:
-#     name = sys.argv[1]
-#   else:
-#     name = 'World'
-#   print('Greetings', name)
-
-# if __name__ == '__main__':
-#   main()
-
-
+if __name__ == '__main__':
+  main()
+# Код, который выполняется только при прямом запуске (например, main()),
+# и код (функции, классы), который можно использовать при импорте, не вызывая побочных эффектов.
+# «Если этот файл запущен напрямую (а не импортирован), то вызови функцию main()».
 
 # def sum_num(a, b):
 #    return a + b
@@ -22,11 +20,11 @@
 
 # practice
 
-num_don = int(input("How many donuts?"))
-# print(type(num_don))
-if num_don >10:
-    print("A lot. Eat less")
-elif num_don == 0: 
-    print("find more donuts")
-else:
-    print("You have enough")
+# num_don = int(input("How many donuts?"))
+# # print(type(num_don))
+# if num_don >10:
+#     print("A lot. Eat less")
+# elif num_don == 0: 
+#     print("find more donuts")
+# else:
+#     print("You have enough")
