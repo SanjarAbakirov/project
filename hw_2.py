e1880c70656a1a824a03e9d588147e349e0fca7b
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2025-10-17 10:40:20 +0600

    iterrations with Hw-2

--- hw_2.py
+++ hw_2.py
@@ -1,13 +1,11 @@
 import sys
 
 def main():
-  # Get the name from the command line, using 'World' as a fallback.
   if len(sys.argv) >= 2:
     name = sys.argv[1]
   else:
     name = 'World'
-  print('Hello', name)
+  print('Greetings', name)
 
-# This is the standard boilerplate that calls the main() function.
 if __name__ == '__main__':
   main()
