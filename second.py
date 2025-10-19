0dd4a919938ac79d5fd412d58bc981b161cfaffa
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2025-10-19 23:52:29 +0600

    added second greet

--- second.py
+++ second.py
@@ -1,3 +1,7 @@
 def greetings(name):
     print(f"Hello , {name}! Welcome to python")
 greetings("Sammy")
+
+def greetings2(name):
+    print(f"Hello , {name}! Welcome to python")
+greetings("Sammy")
