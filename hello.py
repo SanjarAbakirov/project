16189d8b335b03b3445ae1c6a8d7f02aa3faf420
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2025-10-21 15:38:47 +0600

    chenged the location of donuts function

--- hello.py
+++ hello.py
@@ -66,3 +66,20 @@ def main():
 if __name__ == '__main__':
   main()
 
+
+# Donuts function
+# import sys
+
+def main():
+  num_don = int(input('How many donuts?'))
+  if num_don <= 9:
+    # num_don = sys.argv[1]
+    print("Number of donuts:", + num_don),
+  else:
+    print("You eat too many of them")
+
+if __name__ == '__main__':
+# Код, который выполняется только при прямом запуске (например, main()),
+# и код (функции, классы), который можно использовать при импорте, не вызывая побочных эффектов.
+# «Если этот файл запущен напрямую (а не импортирован), то вызови функцию main()».
+  main()
