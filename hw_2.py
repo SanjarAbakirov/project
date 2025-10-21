c10e60d5216a037e14ce7767cc84efe377d918d7
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2025-10-21 19:06:27 +0600

    new task

--- hw_2.py
+++ hw_2.py
@@ -1,21 +1,37 @@
-# palindrome
+# A match_ends
 
 words = ["level", "property", "aza", "kazak", "python", "lesha", "amanda", "kyshtak", "sas"]
 
-def check_fn(words):
+def match_ends(words):
   arr = []
   for word in words:
       if word[0] != word[-1]:
         arr.append(word)
   return arr
-check_fn(words)
-result = check_fn(words)
+match_ends(words)
+result = match_ends(words)
 print(result)
 
 # 
 
- 
 
+
+
+
+
+
+# Ex with the palindrome
+# words = ["level", "property", "aza", "kazak", "python", "lesha", "amanda", "kyshtak", "sas"]
+
+# def match_ends(words):
+#   arr = []
+#   for word in words:
+#       if word[0] != word[-1]:
+#         arr.append(word)
+#   return arr
+# match_ends(words)
+# result = match_ends(words)
+# print(result)
 
 
 
