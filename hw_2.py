bad106505d0e3d334fe2541ce6aa8882e7bc0b15
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2025-10-26 00:00:12 +0600

    back space

--- hw_2.py
+++ hw_2.py
@@ -83,7 +83,8 @@ if main(3) == 27:
         prefix = 'Ok'
 else:
         prefix = 'X'
-# print comment
+print('%s got: %s expected: %s' % (prefix, repr(main(3)), repr(27)))
+
 if __name__ == '__main__':
   unittest.main()
 
